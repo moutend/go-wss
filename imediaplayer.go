60a07aca8630029e55518248905fec85c8a23ea6
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-06-09 14:12:39 +0900

    Add

---
+++ imediaplayer.go
@@ -0,0 +1,60 @@
+package wss
+
+import (
+	"unsafe"
+
+	"github.com/go-ole/go-ole"
+)
+
+type IMediaPlayer struct {
+	ole.IInspectable
+}
+
+type IMediaPlayerVtbl struct {
+	ole.IInspectableVtbl
+	GetAutoPlay                      uintptr
+	PutAutoPlay                      uintptr
+	GetNaturalDuration               uintptr
+	GetPosition                      uintptr
+	PutPosition                      uintptr
+	GetBufferingProgress             uintptr
+	GetCurrentState                  uintptr
+	GetCanSeek                       uintptr
+	GetCanPause                      uintptr
+	GetIsLoopingEnabled              uintptr
+	PutIsLoopingEnabled              uintptr
+	GetIsProtected                   uintptr
+	GetIsMuted                       uintptr
+	PutIsMuted                       uintptr
+	GetPlaybackRate                  uintptr
+	PutPlaybackRate                  uintptr
+	GetVolume                        uintptr
+	PutVolume                        uintptr
+	GetPlaybackMediaMarkers          uintptr
+	AddMediaOpened                   uintptr
+	RemoveMediaOpened                uintptr
+	AddMediaEnded                    uintptr
+	RemoveMediaEnded                 uintptr
+	AddMediaFailed                   uintptr
+	RemoveMediaFailed                uintptr
+	AddCurrentStateChanged           uintptr
+	RemoveCurrentStateChanged        uintptr
+	AddPlaybackMediaMarkerReached    uintptr
+	RemovePlaybackMediaMarkerReached uintptr
+	AddMediaPlayerRateChanged        uintptr
+	RemoveMediaPlayerRateChanged     uintptr
+	AddVolumeChanged                 uintptr
+	RemoveVolumeChanged              uintptr
+	AddSeekCompleted                 uintptr
+	RemoveSeekCompleted              uintptr
+	AddBufferingStarted              uintptr
+	RemoveBufferingStarted           uintptr
+	AddBufferingEnded                uintptr
+	RemoveBufferingEnded             uintptr
+	Play                             uintptr
+	Pause                            uintptr
+}
+
+func (v *IMediaPlayer) VTable() *IMediaPlayerVtbl {
+	return (*IMediaPlayerVtbl)(unsafe.Pointer(v.RawVTable))
+}
