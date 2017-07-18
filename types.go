0390918b9eda57f8c194adabbf7ab6cd3ae92168
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-07-18 17:40:56 +0900

    Add VoiceInformation

--- types.go
+++ types.go
@@ -1,3 +1,14 @@
 package wss
 
+// VoiceGender represents gender of voice.
+// VoiceGender_Male = 0x0
+// VoiceGender_FEMale = 0x1
 type VoiceGender uint32
+
+// VoiceInformation represents characteristic of voice.
+type VoiceInformation struct {
+	Name        string
+	Language    string
+	Description string
+	Gender      VoiceGender
+}
